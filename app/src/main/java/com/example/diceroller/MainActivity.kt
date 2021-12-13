1f8bc887c356090aee731fe3a8d130f3add67464
austinstutzman <alsghs49@gmail.com>
2021-12-13 01:28:03 -0500

    Unit 1: Kotlin basics, Pathway 4, Activity 3 Conditional Behavior In Kotlin (Lucky Number App). CodeLab step 4.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -22,18 +22,27 @@ class MainActivity : AppCompatActivity() {
         //println("Your ${myFirstDice.numSides} sided dice rolled ${rollResult}!")
 
         val luckyNum = 4
-        if (rollResult == luckyNum) {
-            println("You win!")
-        } else if (rollResult == 1) {
-            println("So sorry! You rolled a 1. Try again!")
-        } else if (rollResult == 2) {
-            println("Sadly, you rolled a 2. Try again!")
-        } else if (rollResult == 3) {
-            println("Unfortunately, you rolled a 3. Try again!")
-        } else if (rollResult == 5) {
-            println("Don't cry! You rolled a 5. Try again!")
-        } else {
-            println("Apologies! You rolled a 6. Try again!")
+//        if (rollResult == luckyNum) {
+//            println("You win!")
+//        } else if (rollResult == 1) {
+//            println("So sorry! You rolled a 1. Try again!")
+//        } else if (rollResult == 2) {
+//            println("Sadly, you rolled a 2. Try again!")
+//        } else if (rollResult == 3) {
+//            println("Unfortunately, you rolled a 3. Try again!")
+//        } else if (rollResult == 5) {
+//            println("Don't cry! You rolled a 5. Try again!")
+//        } else {
+//            println("Apologies! You rolled a 6. Try again!")
+//        }
+
+        when (rollResult){
+            luckyNum -> println("You won!")
+            1 -> println("So sorry! You rolled a 1. Try again!")
+            2 -> println("Sadly, you rolled a 2. Try again!")
+            3 -> println("Unfortunately, you rolled a 3. Try again!")
+            5 -> println("Don't cry! You rolled a 5. Try again!")
+            6 -> println("Apologies! You rolled a 6. Try again!")
         }
     }
 
