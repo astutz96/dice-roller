0f92d1fd0284a5777947ba07182eb96694ec7e3d
austinstutzman <alsghs49@gmail.com>
2021-12-14 01:23:04 -0500

    Unit 1: Kotlin basics, Pathway 4, Activity 4 Add images (Dice Roller App). CodeLab step 5.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,46 +12,25 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener { rollDice() }
-
-    }
-
-    private fun doLuckyNum() {
-        val myFirstDice = Dice(8)
-        val rollResult = myFirstDice.roll()
-        //println("Your ${myFirstDice.numSides} sided dice rolled ${rollResult}!")
-
-        val luckyNum = 4
-//        if (rollResult == luckyNum) {
-//            println("You win!")
-//        } else if (rollResult == 1) {
-//            println("So sorry! You rolled a 1. Try again!")
-//        } else if (rollResult == 2) {
-//            println("Sadly, you rolled a 2. Try again!")
-//        } else if (rollResult == 3) {
-//            println("Unfortunately, you rolled a 3. Try again!")
-//        } else if (rollResult == 5) {
-//            println("Don't cry! You rolled a 5. Try again!")
-//        } else {
-//            println("Apologies! You rolled a 6. Try again!")
-//        }
-
-        when (rollResult) {
-            luckyNum -> println("You won!")
-            1 -> println("So sorry! You rolled a 1. Try again!")
-            2 -> println("Sadly, you rolled a 2. Try again!")
-            3 -> println("Unfortunately, you rolled a 3. Try again!")
-            5 -> println("Don't cry! You rolled a 5. Try again!")
-            6 -> println("Apologies! You rolled a 6. Try again!")
-            else -> println("HI")
-        }
     }
 
     private fun rollDice() {
-        val dice = Dice(6)
-        val rollVal: Int = dice.roll()
+        val myFirstDice = Dice(6)
+        val rollResult = myFirstDice.roll()
+
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_1)
-        //val resultTextView: TextView = findViewById(R.id.imageView)
-        //resultTextView.text = rollVal.toString()
+
+        //‘when' expression must be exhaustive, add necessary ‘else' branch.
+        val drawableResource = when (rollResult) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = rollResult.toString()
     }
 }
