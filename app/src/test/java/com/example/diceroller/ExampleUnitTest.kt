491cb50dfd58288e58e4c7c4de8939b25e5c2623
austinstutzman <alsghs49@gmail.com>
2022-08-20 11:47:54 -0400

    Unit 1: Kotlin basics, Pathway 4, Activity 5 Unit tests in android (Dice Roller App). CodeLab step 1-FINAL.

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -17,7 +17,7 @@ class ExampleUnitTest {
 
     @Test
     fun generate_number() {
-        val dice = Dice(1000)
+        val dice = Dice(6)
         val rollResult = dice.roll()
         assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
